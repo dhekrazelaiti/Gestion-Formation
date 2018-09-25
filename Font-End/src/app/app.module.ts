cc472964253313f9676a73160c704f8389f7376d
Dhekra ZELAITI <dhekra.zelaiti@gmail.com>
2018-09-25 14:04:46 +0200

    added design for login page

--- Font-End/src/app/app.module.ts
+++ Font-End/src/app/app.module.ts
@@ -1,21 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatButtonModule,
+  MatCheckboxModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatIconModule,
+  MatCardModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { LoginComponent } from './user/login/login.component';
+const routes: Routes = [
+  {path : 'login', component: UserComponent,
+  children: [{ path: '', component: LoginComponent}]
+  },
+  { path: '' , redirectTo:  '/login', pathMatch: 'full' }
 
+
+];
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatCardModule,
+    FormsModule,
+    MatInputModule,
+    MatIconModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Font-End/src/app/user/user.component.ts
+++ Font-End/src/app/user/user.component.ts
@@ -1,9 +1,51 @@
 import { Component } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+
+
 
 @Component({
   selector: 'app-user',
-  template: ``
+  template: `
+  <body>
+    <div class="pen-title">
+      <h1>Authentication Page</h1>
+    </div>
+  <div class="container">
+      <div class="card">
+          <h1 class="title"> Login</h1>
+            <form>
+              <div class="example-container">
+                  <mat-form-field>
+                      <input matInput placeholder="Enter your email" [formControl]= "email" required>
+                      <mat-icon matPrefix >account_circle</mat-icon>
+                      <mat-error *ngIf="email.invalid">{{getErrorMessage()}}</mat-error>
+                  </mat-form-field><br/><br/>
+                  <mat-form-field>
+                      <input matInput placeholder="Enter your password" [type]="hide ? 'password' : 'text'">
+                      <mat-icon matSuffix (click)="hide = !hide" >{{hide ? 'visibility' : 'visibility_off'}}</mat-icon>
+                  </mat-form-field>
+              </div>
+              <div class="button-container">
+                <button >GO</button>
+              </div>
+              <div class="footer"><a href="#">Forgot your password?</a></div>
+            </form>
+      </div>
+  </div>
+  </body>
+  `
 })
 export class UserComponent {
-  title = 'Font-End';
+
+  hide = true;
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+  getErrorMessage() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+        this.email.hasError('email') ? 'Not a valid email' :
+            '';
+  }
+
 }
+
