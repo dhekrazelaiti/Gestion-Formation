4816a670ec8971e10722e4134175d7dd528774f0
Dhekra ZELAITI <dhekra.zelaiti@gmail.com>
2018-10-02 14:43:21 +0200

    added JWT to authentication API

--- Back_End/server.js
+++ Back_End/server.js
@@ -2,10 +2,11 @@ var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var User = require ('./models/User.js');
+var User = require('./models/User.js');
 var bcrypt = require('bcrypt-nodejs');
 var app = express();
- 
+var jwt = require ('jwt-simple');
+
 app.use(cors());
 app.use(bodyParser.json());
 app.get('/', (req, res) => {
@@ -18,40 +19,41 @@ app.post('/register', (req,res)=>{
   console.log(userData.password);
   console.log(userData.username);
 
-  var user = new User(userData); 
+  var user = new User(userData);
   user.save((err, result) => {
       if (err)
           console.log('saving user error');
-         
+
           res.sendStatus(200);
   });
 });
 app.post('/login', async(req, res)=> {
 
    var loginData = req.body;
-   console.log(loginData);
+   console.log("****login Data:",loginData);
 
-   var user = await User.findOne({email : loginData.email}); 
-
+   var user = await User.findOne({email : loginData.email});
+   console.log("****login Data 2*****:",loginData);
+    
    if(!user)
         return res.status(401).send({message: 'Email or Password invalid'})
 
-    if(loginData.password != user.password)
-        return res.status(401).send({message: 'Password is invalid'});
-       /*
-       var playload = {};
+    bcrypt.compare(loginData.password, user.password, (err, isMatch) => {
+        if(!isMatch)
+          return res.status(401).send({message : "Password is invalid"});
 
-        var token = jwt.encode(playload, '123');
-    
-        res.status(200).send({token : token});
-        console.log("***************",token)
-        */
-       res.status(200).send({message:"okkkkkkkkkk"});
-     });
+        var payload = {};
+
+        var token = jwt.encode(payload, '123');
+        
+         res.status(200).send({token : token});
+         console.log("***************",token)
+      });
+   });
 
 mongoose.connect('mongodb://admin:admin2018@ds245170.mlab.com:45170/gestion_formation', { useNewUrlParser: true }, (err) => {
     if(!err)
         console.log('connected to mongo')
 })
- 
+
 app.listen(3000);
